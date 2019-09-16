f757e28ba71bbbb66f7aa856a42fa9dc262f6ab1
Maarten Hunink <maartenhunink@gmail.com>
2019-09-16 06:39:47 +0200

    add link to name

--- template-parts/practise-centres.php
+++ template-parts/practise-centres.php
@@ -7,23 +7,30 @@
 	if  ($practise_centres) {
 		foreach ($practise_centres  as $practise_centre ) { ?>
 			<div class="<?php echo $col; ?> address" itemscope itemtype="http://schema.org/PostalAddress">
-	    	<p class="address-name"><b><?php echo $practise_centre->name; ?></b></p>
+	    	<p class="address-name">
+	    		<b>
+	    			<?php if (get_field('url', $practise_centre)) : ?><a href="<?php the_field('url', $practise_centre); ?>"><?php endif; ?>
+	    				<?php echo $practise_centre->name; ?>
+	    			<?php if (get_field('url', $practise_centre)) : ?></a><?php endif; ?>
+	    		</b>
+	    	</p>
 	    	<p class="address-address"><?php if(get_field('address_street', $practise_centre)) : ?>
 	    		<span itemprop="streetAddress"><?php the_field('address_street', $practise_centre); ?></span>
 	    	<?php endif;
 	    	if(get_field('address_street_2', $practise_centre)) : ?>
 	    		<span itemprop="streetAddress"><?php the_field('address_street_2', $practise_centre);?></span>
+		    	<br />
 	    	<?php endif; ?>
-	    	<br />
 	    	<?php if(get_field('address_postcode', $practise_centre)) : ?>
 		    	<span itemprop="postalCode"><?php the_field('address_postcode', $practise_centre);?></span>
 		    <?php endif;
 	    	if(get_field('address_city', $practise_centre)) : ?>
 		    	<span itemprop="addressLocality"><?php the_field('address_city', $practise_centre);?></span>
+			    <br />
 		    <?php endif; ?>
-		    <br />
 	    	<?php if(get_field('address_state', $practise_centre)) : ?>
 		    	<span itemprop="addressRegion"><?php the_field('address_state', $practise_centre);?></span>
+			    <br />
 		    <?php endif;
 	    	if(get_field('address_country', $practise_centre)) : ?>
 		    	<span itemprop="addressCountry"><?php the_field('address_country', $practise_centre);?></span>
