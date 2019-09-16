a15128a891d1af857c0a07803011f3b382be5d2f
Maarten Hunink <maartenhunink@gmail.com>
2019-09-16 05:58:18 +0200

    Simple anonymous analytics

--- header.php
+++ header.php
@@ -16,6 +16,17 @@
 	<meta charset="<?php bloginfo( 'charset' ); ?>">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<link rel="profile" href="https://gmpg.org/xfn/11">
+
+	<!-- Global site tag (gtag.js) - Google Analytics -->
+	<script async src="https://www.googletagmanager.com/gtag/js?id=<?php the_field('google_analytics_code', 'options'); ?>"></script>
+	<script>
+	  window.dataLayer = window.dataLayer || [];
+	  function gtag(){dataLayer.push(arguments);}
+	  gtag('js', new Date());
+
+	  gtag('config', <?php the_field('google_analytics_code', 'options'); ?>, { 'anonymize_ip': true });
+	</script>
+	
 	<?php wp_head(); ?>
 </head>
 
