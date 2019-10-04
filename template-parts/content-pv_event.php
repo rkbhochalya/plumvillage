0215c3a3ba3ecd1d02f83bd911270c5d6fc5a3fc
Maarten Hunink <maartenhunink@gmail.com>
2019-10-04 12:54:20 +0200

    fix single events with repeating dates

--- template-parts/content-pv_event.php
+++ template-parts/content-pv_event.php
@@ -18,43 +18,91 @@
 			the_title( '<h2 class="entry-title"><a href="' . esc_url( get_permalink() ) . '" rel="bookmark">', '</a></h2>' );
 		endif;
 		?>
-		<div class="wp-block-image"><figure class="alignright"><?php the_post_thumbnail('landscape'); ?></figure></div>
+		<?php if(has_post_thumbnail()) : ?>
+			<div class="wp-block-image"><figure class="alignright"><?php the_post_thumbnail('landscape'); ?></figure></div>
+		<?php endif; ?>
 
-		<div class="entry-meta">
-					<b class="entry-date">
-						<?php $startDate = new DateTime(get_field('start_date', get_the_ID(), false));?>
-						<?php $endDate = new DateTime(get_field('end_date', get_the_ID(), false));?>
-						<?php echo $startDate->format('M j') . ' - ' . $endDate->format('M j, Y'); ?> 
-					</b>
-					<?php 
-						$terms = wp_get_post_terms(get_the_ID(), 'practise-centres', array('orderby' => 'parent', 'order' => 'DESC'));
-						if(!empty($terms)){
-							echo __('at', 'plumvillage') . ' ';
-							foreach($terms as $term) {
-								if($term->parent == 0){
-									echo $term->name; //do something here
-								}
+		<?php $terms = wp_get_post_terms(get_the_ID(), 'practise-centres', array('orderby' => 'parent', 'order' => 'DESC')); ?>
+		<?php if(!empty($terms) || get_field('start_date', get_the_ID())) : ?>
+			<div class="entry-meta">
+					<?php if(get_field('start_date', get_the_ID())) : ?>
+						<b class="entry-date">
+							<?php $startDate = new DateTime(get_field('start_date', get_the_ID(), false));?>
+							<?php $endDate = new DateTime(get_field('end_date', get_the_ID(), false));?>
+							<?php echo $startDate->format('M j') . ' - ' . $endDate->format('M j, Y'); ?> 
+						</b>
+					<?php endif; ?>
+						<?php 
+							if(!empty($terms)){
+								echo __('at', 'plumvillage') . ' ';
+								foreach($terms as $term) {
+									if($term->parent == 0){
+										echo $term->name; //do something here
+									}
+								} 
 							} 
-						} else {
-							echo 'On Tour';
-						}
-					?>
-					<?php 
-						$terms = wp_get_post_terms(get_the_ID(), 'language');
-						if(!empty($terms)){
-							echo '<br />' . __('Languages', 'plumvillage') . ': ';
-							$i = 0;
-							foreach($terms as $term) {
-								if($i != 0){
-									echo ', ';
-								}
-								echo $term->name; 
-								$i++;
-							} 
-						}
-					?>
-
-		</div><!-- .entry-meta -->
+						?>
+						<?php 
+							$terms = wp_get_post_terms(get_the_ID(), 'language');
+							if(!empty($terms)){
+								echo '<br />' . __('Languages', 'plumvillage') . ': ';
+								$i = 0;
+								foreach($terms as $term) {
+									if($i != 0){
+										echo ', ';
+									}
+									echo $term->name; 
+									$i++;
+								} 
+							}
+						?>
+			</div><!-- .entry-meta -->
+		<?php endif; ?>
+		<?php if(get_field('event_type') == 'day') : ?>
+			<div class="block event-list alignright">
+				<div class="block-inside">
+			<article class='index-event index-item index-event-repeat'>
+				<h4><?php _e('Upcoming dates', 'plumvillage'); ?></h4>
+				<?php $di = 1; ?>
+				<?php $maxItems = 3; ?>
+				<?php while( have_rows('dates', get_the_ID()) ): the_row(); ?>
+					<?php if(get_sub_field('date')) : ?>
+						<?php
+							$date = new DateTime(get_sub_field('date', false));
+							$now = new DateTime();
+							if($date > $now) : 
+								if($maxItems == ($di - 1)) : ?>
+									<div class="load-more-container">
+									 		<div class="center-without-border load-more"><a href="#"><?php _e('Load More', 'plumvillage'); ?></a></div>
+									 		<div class="load-hide">
+									<?php endif; ?>
+									<div class="entry-meta">
+										<b>
+											<?php
+													$practise_centres = get_sub_field('practise_centre');
+													echo $date->format('M j, Y') . ', ';
+													$pi = 1;
+											?>
+										</b>
+											<?php foreach($practise_centres as $practise_centre) { ?>
+							    			<?php if (get_field('url', $practise_centre)) : ?><a href="<?php the_field('url', $practise_centre); ?>"><?php endif; ?>
+							    				<?php echo $practise_centre->name; ?><?php if (get_field('url', $practise_centre)) : ?></a><?php endif; 
+						    				if(count($practise_centres) > $pi) : echo ', '; endif; 
+												$pi++;
+											} ?>
+									</div>
+								<?php if((count(get_field('dates', get_the_ID())) == $di) && ($maxItems < $di)) : ?>
+										 		</div>
+										 	</div>
+								<?php endif; ?>
+								<?php $di++; ?>
+							<?php endif; ?>								
+						<?php endif; ?>
+				<?php endwhile; ?>
+					</div>
+				</div>
+			</article>
+		<?php endif; ?>
 	</header><!-- .entry-header -->
 
 	<div class="entry-content">
--- template-parts/index-day-pv_event.php
+++ template-parts/index-day-pv_event.php
@@ -25,38 +25,42 @@
 				<p><?php echo get_the_limited_excerpt(get_the_ID(), 50); ?></p>
 			</div><!-- .entry-content -->			
 
-				<h4>Upcoming dates</h4>
+				<h4><?php _e('Upcoming dates', 'plumvillage'); ?></h4>
 				<?php $di = 1; ?>
 				<?php $maxItems = 3; ?>
 				<?php while( have_rows('dates', get_the_ID()) ): the_row(); ?>
 					<?php if(get_sub_field('date')) : ?>
-						<?php if($maxItems == ($di - 1)) : ?>
-								 <div class="load-more-container">
-								 		<div class="center-without-border load-more"><a href="#"><?php _e('Load More', 'plumvillage'); ?></a></div>
-								 		<div class="load-hide">
+						<?php
+							$date = new DateTime(get_sub_field('date', false));
+							$now = new DateTime();
+							if($date > $now) : 
+								if($maxItems == ($di - 1)) : ?>
+									<div class="load-more-container">
+									 		<div class="center-without-border load-more"><a href="#"><?php _e('Load More', 'plumvillage'); ?></a></div>
+									 		<div class="load-hide">
+									<?php endif; ?>
+									<div class="entry-meta">
+										<b>
+											<?php
+													$practise_centres = get_sub_field('practise_centre');
+													echo $date->format('M j, Y') . ', ';
+													$pi = 1;
+											?>
+										</b>
+											<?php foreach($practise_centres as $practise_centre) { ?>
+							    			<?php if (get_field('url', $practise_centre)) : ?><a href="<?php the_field('url', $practise_centre); ?>"><?php endif; ?>
+							    				<?php echo $practise_centre->name; ?><?php if (get_field('url', $practise_centre)) : ?></a><?php endif; 
+						    				if(count($practise_centres) > $pi) : echo ', '; endif; 
+												$pi++;
+											} ?>
+									</div>
+								<?php if((count(get_field('dates', get_the_ID())) == $di) && ($maxItems < $di)) : ?>
+										 		</div>
+										 	</div>
+								<?php endif; ?>
+								<?php $di++; ?>
+							<?php endif; ?>								
 						<?php endif; ?>
-							<div class="entry-meta">
-								<b>
-									<?php
-										$date = new DateTime(get_sub_field('date', false));
-										$practise_centres = get_sub_field('practise_centre');
-										echo $date->format('M j, Y') . ', ';
-										$pi = 1;
-									?>
-								</b>
-									<?php foreach($practise_centres as $practise_centre) { ?>
-					    			<?php if (get_field('url', $practise_centre)) : ?><a href="<?php the_field('url', $practise_centre); ?>"><?php endif; ?>
-					    				<?php echo $practise_centre->name; ?><?php if (get_field('url', $practise_centre)) : ?></a><?php endif; 
-				    				if(count($practise_centres) > $pi) : echo ', '; endif; 
-										$pi++;
-									} ?>
-							</div>
-					<?php endif; ?>
-						<?php if((count(get_field('dates', get_the_ID())) == $di) && ($maxItems < $di)) : ?>
-								 		</div>
-								 	</div>
-						<?php endif; ?>
-						<?php $di++; ?>
 				<?php endwhile; ?>
 
 			<?php if(get_comments_number() > 0) : ?>
