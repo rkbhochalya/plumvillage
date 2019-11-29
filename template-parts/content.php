21b66e586eb5072d55df925cd5f6967ebaccd780
Maarten Hunink <maartenhunink@gmail.com>
2019-11-29 11:10:30 +0100

    put date on top for posts

--- template-parts/content.php
+++ template-parts/content.php
@@ -11,21 +11,19 @@
 
 <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
 	<header class="entry-header"> 
+		<?php if ( get_post_type() === 'post' ) : ?>
+			<div class="entry-meta">
+				<?php plumvillage_posted_on(); ?>
+			</div><!-- .entry-meta -->
+		<?php endif; ?>
+
 		<?php
 		if ( is_singular() ) :
 			the_title( '<h1 class="entry-title">', '</h1>' );
 		else :
 			the_title( '<h2 class="entry-title"><a href="' . esc_url( get_permalink() ) . '" rel="bookmark">', '</a></h2>' );
 		endif;
-
-		if ( 'post' === get_post_type() ) :
-			?>
-			<div class="entry-meta">
-				<?php
-				plumvillage_posted_on();
-				?>
-			</div><!-- .entry-meta -->
-		<?php endif; ?>
+		?>
 	</header><!-- .entry-header -->
 
 	<div class="entry-content">
