fefeb8e066d6b7b92105636af574addaae39df0f
Maarten Hunink <maartenhunink@gmail.com>
2019-11-30 12:15:43 +0100

    native sticky posts

--- template-parts/blocks/post_list.php
+++ template-parts/blocks/post_list.php
@@ -6,31 +6,9 @@
 		$maxPosts = get_field('max_posts') ? get_field('max_posts') : 3;
 		$category = get_field('category') ? get_field('category') : 0;
 
+		$sticky = get_option( 'sticky_posts' );
+
 		if(get_field('show') == 'latest') : 
-
-			// Get Sticky posts
-			$sticky_post_ids = get_posts(array(
-				'fields'								 => 'ids',
-				'posts_per_page'				 => $maxPosts,
-				'post_type'							 => 'post',
-				'order'                  => 'DESC',
-				'orderby'                => 'date',
-				'meta_key'							 => 'sticky',
-				'meta_value'						 => 1,
-				'category__in'					 => $category,
-				'suppress_filters' 			 => 0,
-			));
-
-			$sticky_tnh_ids = get_posts(array(
-				'fields'								 => 'ids',
-				'posts_per_page'				 => $maxPosts,
-				'post_type'							 => 'tnh_update',
-				'order'                  => 'DESC',
-				'orderby'                => 'date',
-				'meta_key'							 => 'sticky',
-				'meta_value'						 => 1,
-				'suppress_filters' 			 => 0,
-			));
 
 
 			// Get all posts
@@ -42,13 +20,23 @@
 				'orderby'                => 'date',
 				'category__in'					 => $category,
 				'posts_per_page' 				 => $maxPosts,
-				'exclude'								 => $sticky_post_ids,
+				'exclude'								 => $sticky,
 				'suppress_filters' 			 => 0,
 			));
 
+			$sticky_post_ids = get_posts( array(
+				'fields'								 => 'ids',
+				'post_type'              => 'post',
+				'post_status'            => array( 'publish' ),
+				'order'                  => 'DESC',
+				'orderby'                => 'date',
+				'category__in'					 => $category,
+				'post__in'							 => $sticky,
+				'suppress_filters' 			 => 0,
+			));
 
 			if(get_field('include_tnh_news')) :
-				
+
 				$tnh_ids = get_posts( array(
 					'fields' 								 => 'ids',
 					'post_type'              => 'tnh_update',
@@ -56,30 +44,58 @@
 					'order'                  => 'DESC',
 					'orderby'                => 'date',
 					'posts_per_page' 				 => $maxPosts,
-					'exclude'								 => $sticky_tnh_ids,
+					'exclude'								 => $sticky,
 					'suppress_filters' 			 => 0,
 				));
 
-				$post_ids = array_merge( $sticky_tnh_ids, $sticky_post_ids, $post_ids, $tnh_ids);
+				$sticky_tnh_ids = get_posts( array(
+					'fields'								 => 'ids',
+					'post_type'              => 'tnh_update',
+					'post_status'            => array( 'publish' ),
+					'order'                  => 'DESC',
+					'orderby'                => 'date',
+					'post__in'							 => $sticky,
+					'suppress_filters' 			 => 0,
+				));
+				
+				$post_ids = array_merge( $tnh_ids, $post_ids);
+				$sticky_post_ids = array_merge( $sticky_tnh_ids, $sticky_post_ids);
 
 			endif;
 
-			// the main query
-			$posts = new WP_Query(array(
-			    'post_type' 			=> 'any',
-			    'post__in'  			=> $post_ids, 
-			    'meta_key'				=> 'sticky',
-			    'orderby'   			=> array(
-			    	'meta_value' => 'DESC',
-			    	'date'  => 'DESC',
-			    ), 
-					'posts_per_page' 	=> $maxPosts,
+			// sticky posts query
+			$sticky_posts = new WP_Query(array(
+			    'post_type' 						=> 'any',
+			    'post__in'  						=> $sticky_post_ids, 
+			    'orderby'   						=> 'date',
+					'order'           			=> 'DESC',
+					'ignore_sticky_posts' 	=> 1,					
+					'posts_per_page' 				=> $maxPosts,
 			));
+
+
+			$maxPosts = ($maxPosts - count($sticky_post_ids) > 0) ? $maxPosts - count($sticky_post_ids) : false;
+
+
+			if($maxPosts){
+				// the main query
+				$posts = new WP_Query(array(
+				    'post_type' 						=> 'any',
+				    'post__in'  						=> $post_ids, 
+				    'orderby'   						=> 'date',
+						'order'           			=> 'DESC',
+						'ignore_sticky_posts' 	=> 1,					
+						'posts_per_page' 				=> $maxPosts,
+				));				
+			} else {
+				$posts = new WP_Query(array('post__in' => array(0)));
+			}
 
 		else :
 			$args = array (
-				'post_status'            => array( 'publish' ),
-				'post__in'							=> get_field('posts_to_show')
+				'post_status'           => array( 'publish' ),
+				'post__in'							=> get_field('posts_to_show'),
+				'ignore_sticky_posts' 	=> 1,					
 			);
 			$posts = new WP_Query( $args );
 
@@ -87,9 +103,13 @@
 
 
 		// The Loop
-		if ( $posts->have_posts() ) {  ?>
+		if ( $sticky_posts->have_posts() || $posts->have_posts() ) {  ?>
 
 			<?php if($style == 'list') : ?><ul class="small post-list"><?php else : ?><div class="post-list"><?php endif; ?>
+				<?php while ( $sticky_posts->have_posts() ) { ?>
+					<?php $sticky_posts->the_post(); ?>
+					<?php get_template_part( 'template-parts/'.$style, get_post_type() ); ?>
+				<?php } ?>
 				<?php while ( $posts->have_posts() ) { ?>
 					<?php $posts->the_post(); ?>
 					<?php get_template_part( 'template-parts/'.$style, get_post_type() ); ?>
