4a6d4af0bdef88daafb36e36a19444abb046fdc3
Maarten Hunink <maartenhunink@gmail.com>
2019-12-07 11:10:26 +0100

    books archive page

--- archive-pv_book.php
+++ archive-pv_book.php
@@ -15,26 +15,22 @@
 get_header();
 ?>
 	<div class="container">
-		<?php
-			while ( have_posts() ) :
-				the_post(); ?>
 			<div class="row">
 				<div class="col-md-3 d-none d-md-block">
 					<?php 
-					wp_nav_menu(
-						array(
-							'theme_location' => 'mega-menu',
-							'menu_id' => 'side-menu',
-							'sub_menu' => true,
-							'show_parent' => true
-						)
-					);
-				?>				
+						wp_nav_menu(
+							array(
+								'theme_location' => 'mega-menu',
+								'menu_id' => 'side-menu',
+								'sub_menu' => true,
+								'show_parent' => true
+							)
+						);
+					?>				
 				</div>
 				<div class="col-md-9">
 					<div id="primary" class="content-area">
 						<main id="main" class="site-main">
-							<?php get_template_part( 'template-parts/content', 'page' ); ?>
 						</main><!-- #main -->
 					</div><!-- #primary -->
 					<?php if ( comments_open() || get_comments_number() ) : ?>
@@ -43,7 +39,6 @@ get_header();
 							<?php comments_template(); ?>
 						</div>
 					<?php endif; ?>
-					<?php endwhile; ?>
 
 					<?php 
 					// WP_Query arguments
