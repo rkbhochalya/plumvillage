30db76119513b83386ad06775653a8ffb77e1363
Maarten Hunink <maartenhunink@gmail.com>
2020-01-05 10:07:05 +0100

    a bit more space underneath books

--- template-parts/index-book.php
+++ template-parts/index-book.php
@@ -11,7 +11,7 @@
 
 <article id="post-<?php the_ID(); ?>" class="index-item index-book col-6 col-lg-4 col-xxl-3 <?php echo implode(' ', get_all_term_classes(get_the_id(), 'topics')); ?> <?php echo implode(' ', get_all_term_classes(get_the_id(), 'genre')); ?>">
 	<div class="row">
-		<div class="col-12"><a href="<?php the_permalink(); ?>"><?php the_post_thumbnail('medium') ?></a></div>
+		<div class="col-12 mb-2"><a href="<?php the_permalink(); ?>"><?php the_post_thumbnail('medium') ?></a></div>
 		<div class="col-12">
 			<header class="entry-header">
 				<?php the_title( '<h5 class="entry-title"><a href="' . esc_url( get_permalink() ) . '" rel="bookmark">', '</a></h5>' ); ?>
