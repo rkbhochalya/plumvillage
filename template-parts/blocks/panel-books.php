da7ce71f1df3bcafc6b2eeb5fb787dbf97fc4d1a
Maarten Hunink <maartenhunink@gmail.com>
2020-01-05 12:09:36 +0100

    subtitle at each book within book panels

--- template-parts/blocks/panel-books.php
+++ template-parts/blocks/panel-books.php
@@ -14,10 +14,16 @@ if( have_rows('books') ): ?>
                 <div class="row <?php if(($block['align'] == 'wide') && (count(get_field('books')) > 1)) : ?>justify-content-center<?php endif; ?>">
                     <?php while ( have_rows('books') ) : the_row(); ?>
                         <?php $bookId = get_sub_field('book'); ?>
-                        <div class="<?php if($block['align'] != 'wide') : ?>col <?php if(count(get_field('books')) == 1) : ?>single-book<?php endif; ?><?php else : ?>col-6 col-md-3 <?php endif; ?> mb-4">
+                        <div class="index-item index-book <?php if($block['align'] != 'wide') : ?>col <?php if(count(get_field('books')) == 1) : ?>single-book<?php endif; ?><?php else : ?>col-6 col-md-3 <?php endif; ?> mb-4">
                             <a class="link-zoom" data-fancybox="<?php echo $block['id']; ?>" data-type="ajax" data-touch="false" href="<?php echo get_permalink($bookId); ?>"><span class="book-cover"><span class="icon icon-bg icon-zoom"></span><?php echo get_the_post_thumbnail($bookId, 'medium'); ?></span>
                             <?php if(get_field('show_excerpt')) : ?>
                                 <h5><span><?php echo get_the_title($bookId); ?><span></h5>
+                                <?php if(get_field('subtitle', $bookId)) : ?>
+                                    <div class="entry-meta">
+                                        <?php the_field('subtitle', $bookId); ?>
+                                    </div><!-- .entry-meta -->
+                                <?php endif; ?>
+
                                 <?php if(get_sub_field('excerpt')) : ?>
                                     <p><?php echo get_sub_field('excerpt'); ?></p>
                                 <?php else : ?>
