4e08997f7ffde60b6df4ff1bc8fd91880a702cff
Maarten Hunink <maartenhunink@gmail.com>
2020-02-14 14:44:07 +0100

    fix new font and blockquote block

--- assets/js/gutenberg.js
+++ assets/js/gutenberg.js
@@ -1,15 +1,34 @@
-wp.blocks.registerBlockStyle('core/cover', [
-	{
-		name: 'default',
-		label: 'Default',
-		isDefault: true,
-	},
-	{
-		name: 'align-left',
-		label: 'Align Left'
-	},
-	{
-		name: 'with-background',
-		label: 'With Background'
-	}
-]);
+wp.domReady( () => {
+	wp.blocks.unregisterBlockStyle( 'core/pullquote', 'solid-color' );
+
+	wp.blocks.registerBlockStyle('core/pullquote', [
+		{
+			name: 'default',
+			label: 'Default',
+			isDefault: true,
+		},
+		{
+			name: 'no-padding',
+			label: 'No Padding'
+		}
+	]);
+
+
+	wp.blocks.registerBlockStyle('core/cover', [
+		{
+			name: 'default',
+			label: 'Default',
+			isDefault: true,
+		},
+		{
+			name: 'align-left',
+			label: 'Align Left'
+		},
+		{
+			name: 'with-background',
+			label: 'With Background'
+		}
+	]);
+
+
+} );
